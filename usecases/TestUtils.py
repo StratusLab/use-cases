176de0bf2ae348da1380226f15df7e57bb91c6f9
Charles Loomis <cal@sixsq.com>
2011-11-20 08:13:19 +0100

    start adding utilities to utility class

--- usecases/TestUtils.py
+++ usecases/TestUtils.py
@@ -1,1 +1,10 @@
 
+import os
+import os.path
+
+def which(file):
+    for path in os.environ["PATH"].split(os.pathsep):
+        if file in os.listdir(path):
+            print "%s/%s" % (path, file)
+
+
--- usecases/testClientHelpOption.py
+++ usecases/testClientHelpOption.py
@@ -1,8 +1,12 @@
 import unittest
+import os, os.path
 
-import usecases.TestUtils
+from usecases.TestUtils import *
 
 class testClientHelpOption(unittest.TestCase):
+
+    def _execute_help_option(self, file):
+        print which(file)
 
     def setUp(self):
         print "setUp"
@@ -11,7 +15,9 @@ class testClientHelpOption(unittest.TestCase):
         print "tearDown"
 
     def test_something(self):
-        print "test_something"
+        dir = '/Users/loomis/stratuslab/bin'
+        for f in os.listdir(dir):
+            self._execute_help_option(f)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(testClientHelpOption)
 unittest.TextTestRunner(verbosity=2).run(suite)
