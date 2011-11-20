7afba19fac1e24ea47b6e9a071f59ebf3c90f8cf
Charles Loomis <cal@sixsq.com>
2011-11-20 08:16:44 +0100

    add execute utility

--- usecases/TestUtils.py
+++ usecases/TestUtils.py
@@ -7,4 +7,56 @@ def which(file):
         if file in os.listdir(path):
             print "%s/%s" % (path, file)
 
+def execute(cmd, returnType=None, exit=True, quiet=False, shell=False):
+    printCmd(' '.join(cmd))
+    if quiet:
+        devNull = open('/dev/null', 'w')
+        stdout = devNull
+        stderr = devNull
+    else:
+        stdout = subprocess.PIPE
+        stderr = subprocess.PIPE
+    p = subprocess.Popen(cmd, stdout=stdout, stderr=stderr, shell=shell)
+    p.wait()
+    if quiet:
+        devNull.close()
+    if returnType:
+        return p.returncode
+    else:
+        out = p.stdout.read()
+        err = p.stderr.read()
+        if p.returncode == 0:
+            if not quiet:
+                if out:
+                    printAndFlush(out + '\n')
+                if err:
+                    printAndFlush(err + '\n')
+            return out
+        else:
+            printAndFlush('  [ERROR] Error executing command!\n')
+            if out:
+                printAndFlush(out + '\n')
+            if err:
+                printAndFlush(err + '\n')
+            if exit:
+                raise Exception
+
+def printAndFlush(msg):
+    sys.stdout.flush()
+    print msg,
+    sys.stdout.flush()
+
+def printAction(msg):
+    self.printAndFlush('\n :::%s:::\n' % (':' *len(msg)))
+    self.printAndFlush(' :: %s ::\n' % msg)
+    self.printAndFlush(' :::%s:::\n' % (':' *len(msg)))
+
+def printStep(msg):
+    self.printAndFlush(' :: %s\n' % msg)
+
+def printCmd(msg):
+    self.printAndFlush('  [Executing] %s\n' % msg)
+
+
+
 
