ec113543574e23676766f69832d2ff9526e15b8d
Charles Loomis <cal@sixsq.com>
2013-05-19 12:23:12 +0200

    re-enable the cernvm tests; metadata now references copy at LAL

--- runTests.sh
+++ runTests.sh
@@ -21,8 +21,6 @@ nosetests -v --with-xunit \
   usecases.testPersistentDiskRetainsData \
   usecases.testRunVmFromPersistentDisk \
   usecases.testCloudInitCentOS \
-  usecases.testReadonlyDataDisk
-
-# Don't run CernVM tests because the image takes too long to download.
-#  usecases.testBasicVmLifecycleCernVM \
-#  usecases.testCernVmIsAccessibleViaSsh
+  usecases.testReadonlyDataDisk \
+  usecases.testBasicVmLifecycleCernVM \
+  usecases.testCernVmIsAccessibleViaSsh
