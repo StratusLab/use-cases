205b2cf11ce2da488f54fa08d657cf32692edd87
Charles Loomis <cal@sixsq.com>
2013-06-05 21:28:39 +0200

    add back most tests

--- runTests.sh
+++ runTests.sh
@@ -1,39 +1,30 @@
 #!/bin/sh
 
-# Minimum tests to get handle on SSL errors.
 nosetests -v --with-xunit \
   usecases.testAAACopyMetadataEntries \
+  usecases.testClientHelpOption \
+  usecases.testClientVersionOption \
+  usecases.testMetadataSignAndValidate \
+  usecases.testMetadataUploadAndDeprecate \
+  usecases.testPrepareContext \
+  usecases.testBasicPersistentDiskLifecycle \
+  usecases.testBasicPersistentDiskLifecycleWithTag \
+  usecases.testUploadImage \
+  usecases.testDownloadImageFromMarketplace \
   usecases.testBasicVmLifecycleTtylinux \
   usecases.testBasicVmLifecycleUbuntu \
   usecases.testBasicVmLifecycleCentOS \
-  usecases.testVmIsAccessibleViaSsh
+  usecases.testVmIsAccessibleViaSsh \
+  usecases.testVmStateNotification \
+  usecases.testAttachAndDetachDisk \
+  usecases.testKillReleasesAttachedDisk \
+  usecases.testPersistentDiskRetainsData \
+  usecases.testRunVmFromPersistentDisk \
+  usecases.testCloudInitCentOS \
+  usecases.testReadonlyDataDisk
+  usecases.testBasicVmLifecycleCernVM \
+  usecases.testCernVmIsAccessibleViaSsh
 
-#nosetests -v --with-xunit \
-#  usecases.testAAACopyMetadataEntries \
-#  usecases.testClientHelpOption \
-#  usecases.testClientVersionOption \
-#  usecases.testMetadataSignAndValidate \
-#  usecases.testMetadataUploadAndDeprecate \
-#  usecases.testPrepareContext \
-#  usecases.testBasicPersistentDiskLifecycle \
-#  usecases.testBasicPersistentDiskLifecycleWithTag \
-#  usecases.testUploadImage \
-#  usecases.testDownloadImageFromMarketplace \
-#  usecases.testBasicVmLifecycleTtylinux \
-#  usecases.testBasicVmLifecycleUbuntu \
-#  usecases.testBasicVmLifecycleCentOS \
-#  usecases.testVmIsAccessibleViaSsh \
-#  usecases.testVmStateNotification \
-#  usecases.testAttachAndDetachDisk \
-#  usecases.testKillReleasesAttachedDisk \
-#  usecases.testPersistentDiskRetainsData \
-#  usecases.testRunVmFromPersistentDisk \
-#  usecases.testCloudInitCentOS \
-#  usecases.testReadonlyDataDisk
-
-# Turn off to reduce job time.
-#  usecases.testBasicVmLifecycleCernVM \
-#  usecases.testCernVmIsAccessibleViaSsh \
 #  usecases.testCreateImage
 
 # Currently not working because additional work on server is needed.
