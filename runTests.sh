1e3b7de2428f43cc3154f79db23544303f35b40d
Charles Loomis <cal@sixsq.com>
2013-06-16 07:19:49 +0200

    put back create image test

--- runTests.sh
+++ runTests.sh
@@ -23,10 +23,8 @@ nosetests -v --with-xunit \
  usecases.testCloudInitCentOS \
  usecases.testReadonlyDataDisk \
  usecases.testBasicVmLifecycleCernVM \
- usecases.testCernVmIsAccessibleViaSsh
-
-# Disable for debugging.
-# usecases.testCreateImage
+ usecases.testCernVmIsAccessibleViaSsh \
+ usecases.testCreateImage
 
 # Currently not working because additional work on server is needed.
 # See storage issue #19
